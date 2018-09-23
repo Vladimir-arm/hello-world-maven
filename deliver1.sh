1e6693799ea14fa670be0d4fff3028fd5af54559
vladimir <vladimir-arm@mail.ru>
2018-09-23 15:30:55 -0400

    update2 pom and deliver1.sh

--- deliver1.sh
+++ deliver1.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-mvn install
-NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
-VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
-java -jar target/${NAME}-${VERSION}.jar
+mvn  install
+java -jar /root/.m2/repository/com/vk/hello-world-maven/1.0-SNAPSHOT/hello-world-maven-1.0-SNAPSHOT.jar
